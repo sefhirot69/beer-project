e40a63a27c0eda5e66102606fa2688f8dfb62219
sefhirot69 <josalillo@gmail.com>
2022-02-23 21:26:59 +0100

    feat : instalo phpunit y compruebo funcionamiento

---
+++ tests/BeerCatalog/Shared/Infrastructure/GuzzleHttpClientRepositoryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests\BeerCatalog\Shared\Infrastructure;
+
+use PHPUnit\Framework\TestCase;
+
+class GuzzleHttpClientRepositoryTest extends TestCase
+{
+    public function setUp(): void
+    {
+    }
+
+    /**
+     * @test
+     */
+    public function fetchShouldReturnStatusOk()
+    {
+
+        self::assertTrue(true);
+    }
+
+}

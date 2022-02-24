5bf7839fa1fe5a166f8e4abca30799c727160fc6
sefhirot69 <josalillo@gmail.com>
2022-02-24 21:15:23 +0100

    feat : creo los object mother de la capa de dominio

---
+++ tests/Shared/ObjectMother/MotherCreator.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Shared\ObjectMother;
+
+use Faker\Factory;
+use Faker\Generator;
+
+final class MotherCreator
+{
+    private static $faker;
+
+    public static function random() : Generator {
+        return self::$faker = self::$faker ?: Factory::create();
+    }
+}

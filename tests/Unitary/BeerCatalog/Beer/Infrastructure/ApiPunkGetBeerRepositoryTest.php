7a5906e348c6393f48c8886797f981aa4891b11a
josealiaslloret <jmalias@cash-converters.es>
2022-04-03 22:54:57 +0200

    feat : 🟥 creo test de infrastructure para devolver una cerveza

---
+++ tests/Unitary/BeerCatalog/Beer/Infrastructure/ApiPunkGetBeerRepositoryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unitary\BeerCatalog\Beer\Infrastructure;
+
+use App\BeerCatalog\Beer\Domain\Exceptions\BeersNotFoundException;
+use App\BeerCatalog\Shared\Domain\HttpClientDataSource;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class ApiPunkGetBeerRepositoryTest extends TestCase
+{
+    /**
+     * @var HttpClientDataSource|MockObject
+     */
+    private $httpClientMock;
+
+    protected function setUp(): void
+    {
+        $this->httpClientMock = $this->createMock(HttpClientDataSource::class);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldExpectBeerNotFound(): void
+    {
+        // THEN
+        $this->expectException(BeersNotFoundException::class);
+
+        // GIVEN
+        $beer = [];
+
+        $this->httpClientMock
+            ->expects(self::once())
+            ->method('fetch')
+            ->willReturn($beer);
+
+        // WHEN
+
+        $repository = new ApiPunkGetBeerRepository($this->httpClientMock);
+        $repository->get();
+    }
+}

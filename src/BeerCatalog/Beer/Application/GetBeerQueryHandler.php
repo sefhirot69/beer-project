74c1d604e0e6d7ae271056c313d1a2a28879ce8b
josealiaslloret <jmalias@cash-converters.es>
2022-04-03 17:04:47 +0200

    feat : 🟩 creo controller y caso de uso para pasar los test

---
+++ src/BeerCatalog/Beer/Application/GetBeerQueryHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\BeerCatalog\Beer\Application;
+
+class GetBeerQueryHandler
+{
+    public function __invoke(){}
+}
--- src/Controller/Finder/GetBeerController.php
+++ src/Controller/Finder/GetBeerController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller\Finder;
 
+use App\BeerCatalog\Beer\Application\GetBeerQueryHandler;
+use App\BeerCatalog\Shared\Domain\Exceptions\HttpClientException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,15 +13,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 final class GetBeerController extends AbstractController
 {
-    public function __construct()
-    {}
+    public function __construct(private GetBeerQueryHandler $getBeerQueryHandler)
+    {
+    }
 
     #[Route('/beer', name: 'app_get_beer', methods: ['GET'])]
     public function __invoke(): JsonResponse
     {
+        try {
+            $result = ($this->getBeerQueryHandler)();
 
-        return new JsonResponse([], Response::HTTP_OK);
-
+            return new JsonResponse([], Response::HTTP_OK);
+        } catch (HttpClientException $exception) {
+            return new JsonResponse(['error' => $exception->getMessage()], $exception->getCode());
+        }
     }
-
 }

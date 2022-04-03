39e76634f9d1f01f01146971b776d14a4ef9cdaf
josealiaslloret <jmalias@cash-converters.es>
2022-04-03 17:13:36 +0200

    feat : 🟥 creo test caso de uso que devuelve una cerveza

---
+++ tests/Unitary/BeerCatalog/Beer/Application/GetBeerQueryHandlerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Unitary\BeerCatalog\Beer\Application;
+
+use App\BeerCatalog\Beer\Application\GetBeerQueryHandler;
+use App\Tests\Unitary\BeerCatalog\Beer\Domain\BeerMother;
+use PHPUnit\Framework\TestCase;
+
+class GetBeerQueryHandlerTest extends TestCase
+{
+    private GetBeerDataSource|MockObject $getBeerDS;
+
+    protected function setUp(): void
+    {
+        $this->getBeerDS = $this->createMock(GetBeerDataSource::class);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnBeerDto(): void
+    {
+        // GIVEN
+        $beer = BeerMother::randomBeer();
+
+        $this->getBeerDS
+            ->expects(self::once())
+            ->method('get')
+            ->willReturn($beer->mapToDto());
+
+        $result = new GetBeerQueryHandler($this->getBeerDS);
+        $result();
+    }
+}
